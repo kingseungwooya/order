298d78db8d6591f3f03b0e83e92359da05c14315
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2022-12-28 16:21:59 +0900

    rafactor: delegate functionality to an object

--- src/main/java/org/prgrms/kdt/order/Order.java
+++ src/main/java/org/prgrms/kdt/order/Order.java
@@ -7,24 +7,21 @@ public class Order {
     private final UUID orderId;
     private final UUID customerID;
     private final List<OrderItem> orderItems;
-    private long discountAmount;
+    private FixedAmountVoucher fixedAmountVoucher;
     private OrderStatus orderStatus = OrderStatus.ACCEPTED;
 
     public Order(UUID orderId, UUID customerID, List<OrderItem> orderItems, long discountAmount) {
         this.orderId = orderId;
         this.customerID = customerID;
         this.orderItems = orderItems;
-        this.discountAmount = discountAmount;
+        this.fixedAmountVoucher = new FixedAmountVoucher(discountAmount);
     }
 
     public long totalAmount() {
         Long beforeDiscount =  orderItems.stream()
                 .map(i -> i.getProductPrice() * i.getQuantity())
                 .reduce(0L, Long::sum);
-        return beforeDiscount - discountAmount;
-    }
-    public void setDiscountAmount(long discountAmount) {
-        this.discountAmount = discountAmount;
+        return fixedAmountVoucher.discount(beforeDiscount);
     }
 
     public void setOrderStatus(OrderStatus orderStatus) {
