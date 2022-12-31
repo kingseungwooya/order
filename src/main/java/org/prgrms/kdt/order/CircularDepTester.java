5488ba55446692b4872f1a202b5440710fce9f7a
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2022-12-31 20:58:08 +0900

    순환의존관계시 생기는 문제점에 관하여

---
+++ src/main/java/org/prgrms/kdt/order/CircularDepTester.java
@@ -0,0 +1,39 @@
+package org.prgrms.kdt.order;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * A가 B를 참조하고 B가 A를 참조하는 순환 의존관계가 생성되며 Been CurrentlyInCreationException 발생
+ */
+class A {
+    private final B b;
+
+    A(B b) {
+        this.b = b;
+    }
+}
+class B {
+    private final A a;
+
+    B(A a) {
+        this.a = a;
+    }
+}
+class CircularConfig {
+    @Bean
+    public  A a (B b) {
+        return new A(b);
+    }
+    @Bean B b(A a) {
+        return new B(a);
+    }
+
+}
+
+public class CircularDepTester {
+    public static void main(String[] args) {
+        var annotationConfigApplicationContext = new AnnotationConfigApplicationContext(CircularConfig.class);
+
+    }
+}
