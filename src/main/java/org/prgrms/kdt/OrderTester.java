9742727cbc1e0c09c1bf8f998f44f1b75a4ac51e
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-01-06 17:42:48 +0900

    applicationcontext를 통해 resource를 가져오는 방법

--- src/main/java/org/prgrms/kdt/OrderTester.java
+++ src/main/java/org/prgrms/kdt/OrderTester.java
@@ -13,31 +13,54 @@ import org.springframework.beans.factory.annotation.BeanFactoryAnnotationUtils;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.util.Assert;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class OrderTester {
 
     private static final Logger logger = LoggerFactory.getLogger(OrderTester.class);
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         // Configuration Metadata를 java 기반으로~
-        var applicationContext = new AnnotationConfigApplicationContext();
-        applicationContext.register(AppConfiguration.class);
-        var environment = applicationContext.getEnvironment();
-        environment.setActiveProfiles("dev");
-        applicationContext.refresh();
+        var applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        //applicationContext.register(AppConfiguration.class);
+        //var environment = applicationContext.getEnvironment();
+        //environment.setActiveProfiles("dev");
+        //applicationContext.refresh();
         var orderProperties = applicationContext.getBean(OrderProperties.class);
-        System.out.println(MessageFormat.format("version -> {0}", orderProperties.getVersion()));
-        System.out.println(MessageFormat.format("minimumOrderAmount -> {0}", orderProperties.getMinimumOrderAmount()));
-        System.out.println(MessageFormat.format("supportVendors -> {0}", orderProperties.getDescription()));
+        //System.out.println(MessageFormat.format("version -> {0}", orderProperties.getVersion()));
+        //System.out.println(MessageFormat.format("minimumOrderAmount -> {0}", orderProperties.getMinimumOrderAmount()));
+        //System.out.println(MessageFormat.format("supportVendors -> {0}", orderProperties.getDescription()));
         // property 가져와보기
         /*
         var version = environment.getProperty("kdt.version");
         var minimumOrderAmount = environment.getProperty("kdt.minimum-order-amount" , Integer.class);
         var supportVendors = environment.getProperty("kdt.support-vendors", List.class);  */
-       // var description = environment.getProperty("kdt.description");
+        // var description = environment.getProperty("kdt.description");
+        var resource = applicationContext.getResource("classpath:application.yaml");
+        var resource2 = applicationContext.getResource("file:sample.txt");
+        var resource3 = applicationContext.getResource("https://stackoverflow.com/");
+        System.out.println(MessageFormat.format("Resource -> {0}", resource.getClass().getCanonicalName()));
+        var file = resource.getFile();
+        var strings = Files.readAllLines(file.toPath());
+        System.out.println(strings.stream().reduce("", (a, b) -> a + "\n" + b));
+
+        // 외부에서 리소스 가져오기 가져올 때
+
+        var readableByteChannel = Channels.newChannel(resource3.getURL().openStream());
+        var bufferedReader = new BufferedReader(Channels.newReader(readableByteChannel, StandardCharsets.UTF_8));
+        var contents = bufferedReader.lines().collect(Collectors.joining("\n"));
+        System.out.println(contents);
 
 
         var customerID = UUID.randomUUID();
