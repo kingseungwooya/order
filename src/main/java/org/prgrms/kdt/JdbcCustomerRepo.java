616164cd55447a4b6b82927bf194567f7b2af119
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-01-10 19:11:28 +0900

    jdbc 설정

---
+++ src/main/java/org/prgrms/kdt/JdbcCustomerRepo.java
@@ -0,0 +1,13 @@
+package org.prgrms.kdt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.DriverManager;
+
+public class JdbcCustomerRepo {
+    private static final Logger logger = LoggerFactory.getLogger(JdbcCustomerRepo.class);
+    public static void main(String[] args) {
+
+    }
+}
